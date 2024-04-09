fb5ccb74162465cd7f2ee4d7018c5f280f375b7d
Dhruvik Variya <156924642+dhruvik-variya@users.noreply.github.com>
2024-04-09 15:27:15 +0530

    Update Q-1.cpp

--- DSA Coding exam/Ch-5/Q-1.cpp
+++ DSA Coding exam/Ch-5/Q-1.cpp
@@ -10,13 +10,4 @@ int main(){
         cout << i << endl;
 
     return 0;
-}#include<iostream>
-using namespace std;
-
-int main(){
-    int num;
-
-    cin >> num;
-
-    for(int i=1; i<=num; i++)
-        c
+}
